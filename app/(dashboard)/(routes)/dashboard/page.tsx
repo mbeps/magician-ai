6178e7bfa5422accc1ea482c1dc43e8cf2123312
Maruf Bepary <bepary71@gmail.com>
2023-07-20 16:31:41 +0100

    Added initial routes

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+export default function DashboardPage() {
+  return (
+    <Button>Click</Button>
+  )
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type pageProps = {
+	
+};
+
+const page:React.FC<pageProps> = () => {
+	
+	return <div>This is the landing page for this app</div>
+}
+export default page;
