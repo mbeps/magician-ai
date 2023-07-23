aa5f1bc9aecff15fb61b3b42249ec416e3d58f31
Maruf Bepary <bepary71@gmail.com>
2023-07-23 18:28:14 +0100

    Added markdown formatting to code - Installed React Markdown to format text into markdown

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -18,6 +18,7 @@ import { Loader } from "@/components/loader/Loader";
 import { cn } from "@/lib/utils";
 import { UserAvatar } from "@/components/avatar/UserAvatar";
 import { BotAvatar } from "@/components/avatar/BotAvatar";
+import ReactMarkdown from "react-markdown";
 
 type CodeProps = {};
 
@@ -130,7 +131,21 @@ const CodePage: React.FC<CodeProps> = () => {
                 )}
               >
                 {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                <p className="text-sm">{message.content}</p>
+                <ReactMarkdown
+                  components={{
+                    pre: ({ node, ...props }) => (
+                      <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                        <pre {...props} />
+                      </div>
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code className="bg-black/10 rounded-lg p-1" {...props} />
+                    ),
+                  }}
+                  className="text-sm overflow-hidden leading-7"
+                >
+                  {message.content || ""}
+                </ReactMarkdown>
               </div>
             ))}
           </div>
