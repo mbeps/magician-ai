9a79fb4949964088e430fc763f50e95d0c6a1b69
Maruf Bepary <bepary71@gmail.com>
2023-07-25 16:44:46 +0100

    Decreased usage limit from 5 to 3

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,6 +1,6 @@
 import { Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
 
-export const MAX_FREE_COUNTS = 5;
+export const MAX_FREE_COUNTS = 3;
 
 export const tools = [
   {
