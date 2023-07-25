0c873b225948377b98edbfb7b8a80e03f82594d1
Maruf Bepary <bepary71@gmail.com>
2023-07-25 17:44:26 +0100

    Changed gradient colours

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -18,7 +18,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 transition-all bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+      className="h-full w-full flex-1 transition-all bg-gradient-to-r from-green-500 to-blue-500"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
