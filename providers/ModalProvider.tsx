f3852c3a8c565bccc95a6a8363fb0d12234a020b
Maruf Bepary <bepary71@gmail.com>
2023-07-27 13:51:06 +0100

    Added documentation for providers

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -3,8 +3,14 @@
 import { ProModal } from "@/components/modals/ProModal";
 import { useEffect, useState } from "react";
 
+/**
+ * This is a provider for the modal component.
+ * It allows every component to use the modal without having to pass it down.
+ * If the modal is not mounted then it will not be rendered.
+ * @returns (JSX.Element): provider for modal
+ */
 export const ModalProvider = () => {
-  const [isMounted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false); // by default not rendered
 
   // prevent hydration
   useEffect(() => {
