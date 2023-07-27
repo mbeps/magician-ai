6a9fd153e248f9acaa48c8fac99020532b202430
Maruf Bepary <bepary71@gmail.com>
2023-07-27 13:45:28 +0100

    Added documentation for the libs

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,9 @@
 import Stripe from "stripe";
 
+/**
+ * Stripe instance used to make API calls to Stripe.
+ * This allows us to use the Stripe API in our code.
+ */
 export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
   apiVersion: "2022-11-15",
   typescript: true,
--- lib/subscriptions.ts
+++ lib/subscriptions.ts
@@ -3,13 +3,21 @@ import prismadb from "@/lib/prismadb";
 
 const DAY_IN_MS = 86_400_000; // 1 day
 
+/**
+ * Checks whether the user is subscribed or not.
+ * Allows the user to access the API if they are subscribed without any limits.
+ * 1 day is added to the current period end to account for timezones.
+ * @returns (boolean): Whether the user is subscribed or not
+ */
 export const checkSubscription = async () => {
-  const { userId } = auth();
+  const { userId } = auth(); // get the user ID
 
+  // if the user ID is not valid, return false
   if (!userId) {
     return false;
   }
 
+  // get the subscription details for the user
   const userSubscription = await prismadb.userSubscription.findUnique({
     where: {
       userId: userId,
@@ -22,14 +30,16 @@ export const checkSubscription = async () => {
     },
   });
 
+  // if the user does not have a subscription, return false
   if (!userSubscription) {
     return false;
   }
 
+  // if the user has a subscription and the current period end is greater than the current time, return true
   const isValid =
     userSubscription.stripePriceId &&
     userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS >
       Date.now();
 
-  return !!isValid;
+  return !!isValid; // convert to boolean
 };
