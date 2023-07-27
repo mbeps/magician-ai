4f0f4465819a9c667fc77a5c735a69d8e9724463
Maruf Bepary <bepary71@gmail.com>
2023-07-27 13:47:53 +0100

    Added documentation for hooks

--- hooks/useProModal.ts
+++ hooks/useProModal.ts
@@ -6,8 +6,14 @@ interface useProModalStore {
   onClose: () => void;
 }
 
+/**
+ * Global state for the Pro modal component.
+ * This allows multiple components to access the state of the modal.
+ * Components can also update the state of the modal to open it or close it.
+ * @returns (useProModalStore): The state of the Pro modal
+ */
 export const useProModal = create<useProModalStore>((set) => ({
-  isOpen: false,
-  onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
+  isOpen: false, // closed by default
+  onOpen: () => set({ isOpen: true }), // open the modal
+  onClose: () => set({ isOpen: false }), // close the modal
 }));
